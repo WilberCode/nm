72385b2bec63c9666d1398c875e88bcab8654b8f
Wilber <parionahuaraca@gmail.com>
2019-10-03 14:27:13 -0500

    se agrego cambios

--- index.php
+++ index.php
@@ -46,9 +46,18 @@ En departamentos amplios, funcionales y perfectamente distribuidos que se adapta
     <link rel="stylesheet" href="dist/css/tailwindcss-dist.css" />
     <link rel="stylesheet" href="dist/css/style.css" />
     <title>nm espacios</title>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NDMGWRB');</script>
+    <!-- End Google Tag Manager -->
 
-
-
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NDMGWRB"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
   </head>
   <body>
